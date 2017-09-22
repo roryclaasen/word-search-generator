dd9a9f57fa54c9c4a3daa5f1b7b85322776c9c15
Rory Claasen <rory@claasen.biz>
2017-09-22 16:12:35 +0100

    added word list

--- assets/script/gen.js
+++ assets/script/gen.js
@@ -1,15 +1,25 @@
-function addWord() {
-    var list = $('#word-list > ul');
-    var item = $('<li class="list-group-item"></i>')
-    item.html($('#word').text() + '<i class="fa fa-times" aria-hidden="true"></i>');
-    list.append(item);
-}
-
 $(function() {
-    $('#word-list > ul > li').each(function() {
-        var item = $(this);
-        item.find('i.fa-times').click(function() {
-            item.remove();
-        });
+    $('form#addword').on('submit', function(e) {
+        e.preventDefault();
+        var newWord = $('#word').val();
+        if (newWord.length > 0) {
+            var emptyMsg = $('#word-list > ul > li.disabled');
+            emptyMsg.hide();
+
+            var list = $('#word-list > ul');
+            var item = $('<li class="list-group-item"></i>')
+            var remove = $('<i class="fa fa-times" aria-hidden="true"></i>');
+            remove.click(function() {
+                $(this).parent().remove();
+                if ($('#word-list > ul > li').length == 1) {
+                    emptyMsg.show();
+                }
+            });
+            var spanWord = $('<span class="word">' + newWord + '</span>');
+            item.append(spanWord);
+            item.append(remove);
+            list.append(item);
+            $('#word').val("");
+        }
     });
 });
