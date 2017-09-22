eba76b3adb590fbe046d484a554727775010b801
Rory Claasen <rory@claasen.biz>
2017-09-22 22:56:38 +0100

    worked on sizes

--- assets/script/gen.js
+++ assets/script/gen.js
@@ -16,46 +16,78 @@ $(function() {
             var remove = $('<i class="fa fa-times" aria-hidden="true"></i>');
             remove.click(function() {
                 $(this).parent().remove();
-                if ($('#word-list > ul > li').length == 1) {
-                    emptyMsg.show();
-                }
             });
             var spanWord = $('<span class="word">' + newWord.toUpperCase() + '</span>');
             item.append(spanWord);
             item.append(remove);
             list.append(item);
             $('#word').val("");
-            $('span#nowords').hide();
         }
     });
+
+    function fillEmpty(table, width, height) {
+        for (var i = 0; i < height; i++) {
+            for (var x = 0; x < width; x++) {
+                if (table[i][x] == undefined) table[i][x] = randomChar();
+            }
+        }
+        return table;
+    }
+
+    function makeEmptyTable(table, width, height) {
+        for (var y = 0; y < height; y++) {
+            var row = $('<tr></tr>');
+            for (var x = 0; x < width; x++) {
+                var cell = $('<td></td>');
+                row.append(cell);
+            }
+            table.append(row);
+        }
+    }
+
     $('button#generate').click(function() {
-        var width = 12;
-        var height = 12;
-        var noMsg = $('span#nowords');
-        if ($('#word-list > ul > li').length == 1) {
-            noMsg.show();
-        } else {
-            noMsg.hide();
-            var table = $('table#search-output tbody');
-            table.html('');
+        var width = parseInt($('#tableWidth').value());
+        var height =  parseInt($('#tableHeight').value());
+        var table = $('table#search-output tbody');
+        table.html('');
 
-            var charTable = [];
-            for (var i = 0; i < height; i++) {
-                charTable[i] = new Array(width);
-                for (var x = 0; x < width; x++) {
-                    charTable[i][x] = randomChar();
-                }
-            }
+        var charTable = [];
+        for (var i = 0; i < height; i++) {
+            charTable[i] = new Array(width);
+        }
+        if ($('#word-list > ul > li').length > 1) {
+            var wordList = [];
+            $('#word-list > ul > li').each(function() {
+                wordList.push($(this).children('span.word').text());
+            });
 
-            for (var y = 0; y < height; y++) {
-                var row = $('<tr></tr>');
-                for (var x = 0; x < width; x++) {
-                    var cell = $('<td></td>');
-                    cell.html(charTable[y][x]);
-                    row.append(cell);
-                }
-                table.append(row);
+        }
+        charTable = fillEmpty(charTable, width, height);
+
+        for (var y = 0; y < height; y++) {
+            var row = $('<tr></tr>');
+            for (var x = 0; x < width; x++) {
+                var cell = $('<td></td>');
+                cell.html(charTable[y][x]);
+                row.append(cell);
             }
+            table.append(row);
         }
     });
+
+    function sizeChanged() {
+        var width = parseInt($('#tableWidth').text());
+        var height =  parseInt($('#tableHeight').text());
+        var table = $('table#search-output tbody');
+        table.html('');
+
+        var charTable = [];
+        for (var i = 0; i < height; i++) {
+            charTable[i] = new Array(width);
+        }
+        makeEmptyTable(table, width, height);
+    }
+
+    $('#tableWidth').change(sizeChanged());
+    $('#tableHeight').change(sizeChanged());
 });
