ed7a8b0dbfe5c050540a7735dea6a96793474bd7
Rory Claasen <t-roclaa@microsoft.com>
2017-09-28 11:31:25 +0100

    now works in all directions, and input checking

--- assets/script/gen.js
+++ assets/script/gen.js
@@ -1,4 +1,24 @@
+// http://jsfiddle.net/joquery/9KYaQ/
+String.format = function() {
+    var theString = arguments[0];
+    for (var i = 1; i < arguments.length; i++) {
+        var regEx = new RegExp("\\{" + (i - 1) + "\\}", "gm");
+        theString = theString.replace(regEx, arguments[i]);
+    }
+    return theString;
+}
+
 $(function() {
+    $("a[href^='#']").on('click', function(e) {
+        e.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 300, function() {
+            window.location.hash = hash;
+        });
+    });
+
     function randomChar() {
         var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         return possible.charAt(Math.floor(Math.random() * possible.length));
@@ -22,21 +42,30 @@ $(function() {
         e.preventDefault();
         var newWord = $('#word').val();
         if (newWord.length > 0) {
-            var emptyMsg = $('#word-list > ul > li.disabled');
-            emptyMsg.hide();
+            var helpBlock = $('#addword .form-group span.help-block');
+            var formGroup = $('#addword .form-group');
+            if (!/[^a-zA-Z]/.test(newWord)) {
+                var emptyMsg = $('#word-list > ul > li.disabled');
+                emptyMsg.hide();
+                helpBlock.hide();
+                formGroup.removeClass("has-error");
 
-            var list = $('#word-list > ul');
-            var item = $('<li class="list-group-item"></i>')
-            var remove = $('<i class="fa fa-times" aria-hidden="true"></i>');
-            remove.click(function() {
-                $(this).parent().remove();
-                if ($('#word-list > ul > li').length == 1) emptyMsg.show();
-            });
-            var spanWord = $('<span class="word">' + newWord.toUpperCase() + '</span>');
-            item.append(spanWord);
-            item.append(remove);
-            list.append(item);
-            $('#word').val("");
+                var list = $('#word-list > ul');
+                var item = $('<li class="list-group-item"></i>')
+                var remove = $('<i class="fa fa-times" aria-hidden="true"></i>');
+                remove.click(function() {
+                    $(this).parent().remove();
+                    if ($('#word-list > ul > li').length == 1) emptyMsg.show();
+                });
+                var spanWord = $('<span class="word">' + newWord.toUpperCase() + '</span>');
+                item.append(spanWord);
+                item.append(remove);
+                list.append(item);
+                $('#word').val("");
+            } else {
+                helpBlock.show();
+                formGroup.addClass("has-error");
+            }
         }
     });
 
@@ -67,7 +96,7 @@ $(function() {
         table.html('');
 
 
-        console.log("Generating Word Search (" + width + "x" + height + ")");
+        console.log(String.format("Generating Word Search ({0}x{1})", width, height));
 
         var charTable = [];
         for (var i = 0; i < height; i++) {
@@ -87,30 +116,87 @@ $(function() {
                 var sLength = sWord.length;
                 while (attempt < 5 && !placed) {
                     attempt++;
-                    console.log("Attempt " + attempt + " to place '" + sWord + "'");
-                    var x = Math.floor(Math.random() * width);
-                    var y = Math.floor(Math.random() * height);
+                    // console.log("Attempt " + attempt + " to place '" + sWord + "'");
+                    try {
+                        var x = Math.floor(Math.random() * width);
+                        var y = Math.floor(Math.random() * height);
 
-                    // TODO Change gen to support multiple directions
-                    if (x + sLength >= width) x = width - sLength;
-
-                    var canPlace = true;
-                    for (var i = 0; i < sLength; i++) {
-                        if (charTable[y][x + i] == undefined) continue;
-                        if (charTable[y][x + i] == sWord[i]) continue;
-                        canPlace = false;
-                    }
-
-                    if (canPlace) {
-                        for (var i = 0; i < sLength; i++) {
-                            charTable[y][x + i] = sWord[i];
+                        var options = {
+                            vertical: true,
+                            horizontal: true,
+                            diagonal: true
                         }
-                        placed = true;
+
+                        var tx = x, ty = y;
+
+                        if (options.vertical) {
+                            if (y + sLength >= height) ty = height - sLength;
+
+                            for (var i = 0; i < sLength; i++) {
+                                if (charTable[ty + i][tx] == undefined) continue;
+                                if (charTable[ty + i][tx] == sWord[i]) continue;
+                                options.vertical = false;
+                            }
+                        }
+
+                        if (options.horizontal) {
+                            if (x + sLength >= width) tx = width - sLength;
+
+                            for (var i = 0; i < sLength; i++) {
+                                if (charTable[ty][tx + i] == undefined) continue;
+                                if (charTable[ty][tx + i] == sWord[i]) continue;
+                                options.horizontal = false;
+                            }
+                        }
+
+                        if (options.diagonal) {
+                            if (y + sLength >= height) ty = height - sLength;
+                            if (x + sLength >= width) tx = width - sLength;
+
+                            for (var i = 0; i < sLength; i++) {
+                                if (charTable[ty + i][tx + i] == undefined) continue;
+                                if (charTable[ty + i][tx + i] == sWord[i]) continue;
+                                options.diagonal = false;
+                            }
+                        }
+
+                        var finalOptions = {}
+                        if (options.vertical) finalOptions["vertical"] = true;
+                        if (options.horizontal) finalOptions["horizontal"] = true;
+                        if (options.diagonal) finalOptions["diagonal"] = true;
+
+                        var obj_keys = Object.keys(finalOptions);
+                        var ran_key = obj_keys[Math.floor(Math.random() * obj_keys.length)];
+
+                        if (ran_key == "vertical") {
+                            if (y + sLength >= height) y = height - sLength;
+                            for (var i = 0; i < sLength; i++) {
+                                charTable[y + i][x] = sWord[i];
+                            }
+                            placed = true;
+                        } else if (ran_key == "horizontal") {
+                            if (x + sLength >= width) x = width - sLength;
+                            for (var i = 0; i < sLength; i++) {
+                                charTable[y][x + i] = sWord[i];
+                            }
+                            placed = true;
+                        } else if (ran_key == "diagonal") {
+                            if (y + sLength >= height) y = height - sLength;
+                            if (x + sLength >= width) x = width - sLength;
+                            for (var i = 0; i < sLength; i++) {
+                                charTable[y + i][x + i] = sWord[i];
+                            }
+                            placed = true;
+                        }
+
+                        if (placed) console.log(String.format("Placed '{0}' on attempt {1} ({2})", sWord, attempt, ran_key));
+                    } catch (err) {
+                        console.log(err);
                     }
-                }
-                if (!placed) {
-                    // TODO Show missing words in UI
-                    console.log("Failed to place '" + sWord + "' after " + attempt + " attempt(s)");
+                    if (!placed) {
+                        // TODO Show missing words in UI
+                        console.log(String.format("Failed to place '{0}' after {1} attempt(s)", sWord, attempt));
+                    }
                 }
             }
         }
