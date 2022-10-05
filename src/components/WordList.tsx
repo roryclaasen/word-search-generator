5f8f6b06edd60f27602c428a6a8f8444a161c94c
Rory Claasen <contact@roryclaasen.dev>
2022-10-05 23:17:07 +0100

    Solid-JS For component fix (#7)
    <!-- 
    Thanks for creating this pull request 🤗
    
    Please make sure that the pull request is limited to one type (docs,
    feature, etc.) and keep it as small as possible. You can open multiple
    prs instead of opening a huge one.
    -->
    
    <!-- If this pull request closes an issue, please mention the issue
    number below -->
    Closes #6
    
    ## 📑 Description
    Wraps the for component in a conditional to not attempt to render it if
    the list is empty
    
    <!-- You can also choose to add a list of changes and if they have been
    completed or not by using the markdown to-do list syntax
    - [ ] Not Completed
    - [x] Completed
    -->
    
    ## ✅ Checks
    <!-- Make sure your pr passes the CI checks and do check the following
    fields as needed - -->
    - [x] My pull request adheres to the code style of this project
    - [x] My code requires changes to the documentation
    - [x] I have updated the documentation as required
    - [x] All the tests have passed
    
    ## ℹ Additional Information
    <!-- Any additional information like breaking changes, dependencies
    added, screenshots, comparisons between new and old behavior, etc. -->
    
    (This did not happen in local dev)

--- src/components/WordList.tsx
+++ src/components/WordList.tsx
@@ -16,20 +16,23 @@ const WordList: VoidComponent = () => {
 
     return (
         <ul role="list" class="space-y-3 w-full words">
-            {wordList().length === 0 && <li class="text-gray-500">No words added yet</li>}
-            <For each={wordList()}>
-                {(word, index) => (
-                    <li class="flex items-center justify-between">
-                        <span class="uppercase">{word}</span>
-                        <button
-                            class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-1 px-2 border-b-4 border-blue-700 hover:border-blue-500 rounded"
-                            onclick={() => removeWord(index())}
-                        >
-                            X
-                        </button>
-                    </li>
-                )}
-            </For>
+            {wordList().length === 0 ? (
+                <li class="text-gray-500">No words added yet</li>
+            ) : (
+                <For each={wordList()}>
+                    {(word, index) => (
+                        <li class="flex items-center justify-between">
+                            <span class="uppercase">{word}</span>
+                            <button
+                                class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-1 px-2 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+                                onclick={() => removeWord(index())}
+                            >
+                                X
+                            </button>
+                        </li>
+                    )}
+                </For>
+            )}
         </ul>
     );
 };
